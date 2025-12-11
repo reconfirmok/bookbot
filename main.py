456c36be6e454cf9f00afbf96b671bb4b2822aff
Amr <240349211+reconfirmok@users.noreply.github.com>
2025-12-11 22:46:00 +0200

    Read File

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_content = get_book_text("books/frankenstein.txt")
+    print(book_content)
+
+main()
