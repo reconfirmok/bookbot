3b506046e2cbda3183d7e0ab810ee05210f633c6
Amr <240349211+reconfirmok@users.noreply.github.com>
2025-12-11 22:54:58 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_book_words
 
-def get_book_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_content = get_book_text("books/frankenstein.txt")
     num_words = get_book_words(book_content)
     print(f"Found {num_words} total words")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
 
 main()
