a294ea54ecf3364068b73216f92e40f2183e5682
Amr <240349211+reconfirmok@users.noreply.github.com>
2025-12-11 23:57:38 +0200

    Argumnets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_book_words,
     get_num_characters,
@@ -6,10 +7,16 @@ from stats import (
 
 
 def main():
-    book_content = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_content = get_book_text(sys.argv[1])
     num_words = get_book_words(book_content)
     num_chars = get_num_characters(book_content)
     dict_sorted = sorted_dict(num_chars)
+
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at books/frankenstein.txt...")
