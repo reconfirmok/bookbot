800eb691e6a44e937899c7855a09f5b3097a0d85
Amr <240349211+reconfirmok@users.noreply.github.com>
2025-12-11 22:49:42 +0200

    Count words

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 
+def get_book_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
     book_content = get_book_text("books/frankenstein.txt")
-    print(book_content)
+    num_words = get_book_words(book_content)
+    print(f"Found {num_words} total words")
 
 main()
